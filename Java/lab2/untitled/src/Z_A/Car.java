0b7ed0c9887e2f104488e5ffaedb86d3f6599208
jsosnowski <Iplayagame101@gmail.com>
2022-12-19 15:19:45 +0100

    Lab 1 Z_C

--- Java/lab2/untitled/src/Z_A/Car.java
+++ Java/lab2/untitled/src/Z_A/Car.java
@@ -1,12 +1,16 @@
+package Z_A;
+
+import java.util.Scanner;
+
 public class Car {
     private final String carBrand;
     private final int carProdYear;
     private final String carModel;
     private final int doorNumber;
     private final int engineCapacity;
-    private final float avgCombustion;
+    private final double avgCombustion;
 
-    public Car(String carBrand, int carProdYear, String carModel, int doorNumber, int engineCapacity, float avgCombustion) {
+    public Car(String carBrand, int carProdYear, String carModel, int doorNumber, int engineCapacity, double avgCombustion) {
         this.carBrand = carBrand;
         this.carProdYear = carProdYear;
         this.carModel = carModel;
@@ -20,12 +24,28 @@ public class Car {
     public String getCarModel() {return carModel;}
     public int getDoorNumber() {return doorNumber;}
     public int getEngineCapacity() {return engineCapacity;}
-    public float getAvgCombustion() {return avgCombustion;}
+    public double getAvgCombustion() {return avgCombustion;}
 
     @Override
     public String toString() {
         return "Marka pojazdu: " + this.getCarBrand() + "\nRok produkcji: " + this.getCarProdYear() + "\nModel pojazdu: " +
                 this.getCarModel() + "\nLiczba drzwi: " + this.getDoorNumber() + "\nPojemność silnika: " + this.getEngineCapacity()
                 + "\nŚrednie spalanie: " + this.getAvgCombustion();
+    }
+
+    private double countCombustion()
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj długość drogi: ");
+        double roadLength = scan.nextDouble();
+        return roadLength * avgCombustion;
+    }
+
+    public double countDriveCost()
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj cenę paliwa: ");
+        double fuelPrice = scan.nextDouble();
+        return countCombustion() * fuelPrice;
     }
 }
